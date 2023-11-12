1778bf7ae113322592c710dbaf65df736fd9fc2d
Abdurahman Popal <popal.dev@gmail.com>
2023-11-12 16:49:55 +0430

    switch

--- lib/src/extensions/text_theme_extension.dart
+++ lib/src/extensions/text_theme_extension.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 
 extension TextThemeExtension on TextTheme {
-  // TextStyle get caption2 => caption!.copyWith(fontWeight: FontWeight.w600);
-  // TextStyle get caption3 => caption!.copyWith(fontWeight: FontWeight.bold);
-  // TextStyle get headline2Bold =>
-  //     headline2!.copyWith(fontWeight: FontWeight.bold);
-  // TextStyle get headline4Bold =>
-  //     headline4!.copyWith(fontWeight: FontWeight.bold);
-  // TextStyle get bodyText3 => bodyText2!.copyWith(fontWeight: FontWeight.bold);
-  // TextStyle get subtitle2Bold =>
-  //     subtitle2!.copyWith(fontWeight: FontWeight.bold);
+  TextStyle get caption2 => labelMedium!;
+  TextStyle get caption3 => labelLarge!;
+  TextStyle get headline2Bold => headlineLarge!;
+  // TextStyle get headline4Bold => displayLarge!;
+  TextStyle get bodyText3 => bodyLarge!;
+  TextStyle get subtitle2Bold => titleLarge!;
 }
