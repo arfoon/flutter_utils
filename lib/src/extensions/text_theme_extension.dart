f974aa73647aff192f903fd8c66433ccd184b737
Abdurahman Popal <popal.dev@gmail.com>
2023-11-25 14:22:54 +0430

    understandable

--- lib/src/extensions/text_theme_extension.dart
+++ lib/src/extensions/text_theme_extension.dart
@@ -7,4 +7,16 @@ extension TextThemeExtension on TextTheme {
   // TextStyle get headline4Bold => displayLarge!;
   TextStyle get bodyText3 => bodyLarge!;
   TextStyle get subtitle2Bold => titleLarge!;
+
+  TextStyle? get label => labelSmall;
+  TextStyle? get body => bodySmall;
+  TextStyle? get title => titleSmall;
+  TextStyle? get headline => headlineSmall;
+  TextStyle? get display => displaySmall;
+
+  TextStyle? get labelBold => labelLarge;
+  TextStyle? get bodyBold => bodyLarge;
+  TextStyle? get titleBold => titleLarge;
+  TextStyle? get headlineBold => headlineLarge;
+  TextStyle? get displayBold => displayLarge;
 }
