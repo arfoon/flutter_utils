6877e6304fac8d67cb84818d12542a05e2bc7415
Abdurahman Popal <popal.dev@gmail.com>
2024-10-29 10:00:24 +0430

    37

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -4,10 +4,11 @@ import 'package:flutter/foundation.dart';
 
 // export 'package:flutter/foundation.dart' show kIsWeb;
 
-bool kIsIos = !kIsWeb && Platform.isIOS;
-bool kIsMacOs = !kIsWeb && Platform.isMacOS;
-bool kIsWindows = !kIsWeb && Platform.isWindows;
-bool get kIsAndroid => !kIsWeb && Platform.isAndroid;
-bool kIsMobile = Platform.isAndroid || Platform.isIOS;
-bool kIsDesktop = kIsWeb || Platform.isWindows || Platform.isMacOS;
-bool kIsIpod = false;
+bool get kIsIos => kIsWeb ? false : Platform.isIOS;
+bool get kIsMacOs => kIsWeb ? false : Platform.isMacOS;
+bool get kIsWindows => kIsWeb ? false : Platform.isWindows;
+bool get kIsAndroid => kIsWeb ? false : Platform.isAndroid;
+bool get kIsMobile => kIsWeb ? false : Platform.isAndroid || Platform.isIOS;
+bool get kIsDesktop => kIsWeb ? true : Platform.isWindows || Platform.isMacOS;
+bool get kIsDesktopPlatform => Platform.isWindows || Platform.isMacOS;
+bool get kIsIpod => false;
