5c57297035585a8b2d59394690aa43d52f7418b2
Abdurahman Popal <popal.dev@gmail.com>
2026-05-07 12:44:54 +0430

    ok

--- lib/src/app_theme/text_style/color_ext.dart
+++ lib/src/app_theme/text_style/color_ext.dart
@@ -2,14 +2,14 @@ import 'package:flutter_utils/flutter_utils.dart';
 import 'package:flutter_utils/src/app_theme/text_style/text_sizes.dart';
 
 extension ColorUtilsStyleExt on Color {
-  TextStyle? get overline => TextSizes.instance.overline;
-  TextStyle? get caption => TextSizes.instance.caption;
-  TextStyle? get label => TextSizes.instance.label;
-  TextStyle? get body => TextSizes.instance.body;
-  TextStyle? get title => TextSizes.instance.title;
-  TextStyle? get headline => TextSizes.instance.headline;
-  TextStyle? get display => TextSizes.instance.display;
-  TextStyle? get head => TextSizes.instance.head;
+  TextStyle? get overline => TextSizes.instance.overline.copyWith(color: this);
+  TextStyle? get caption => TextSizes.instance.caption.copyWith(color: this);
+  TextStyle? get label => TextSizes.instance.label.copyWith(color: this);
+  TextStyle? get body => TextSizes.instance.body.copyWith(color: this);
+  TextStyle? get title => TextSizes.instance.title.copyWith(color: this);
+  TextStyle? get headline => TextSizes.instance.headline.copyWith(color: this);
+  TextStyle? get display => TextSizes.instance.display.copyWith(color: this);
+  TextStyle? get head => TextSizes.instance.head.copyWith(color: this);
 
   List<TextStyle> get styles => [
         overline,
